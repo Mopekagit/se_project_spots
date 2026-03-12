4f28457da144ae3ba196c50e06e49f4756a8a58a
Matthew <matthew.opeka@gmail.com>
2026-03-12 01:18:45 -0500

    Stage 5 Form Submission - Correction 3

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 const editProfileForm = editProfileModal.querySelector(".modal__form");
 const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
-const editProfileNameDesciptionInput = editProfileModal.querySelector("#profile-description-input");
+const editProfileNameDescriptionInput = editProfileModal.querySelector("#profile-description-input");
 
 
 const newPostBtn = document.querySelector(".profile__add-btn");
@@ -17,32 +17,46 @@ const profileDescriptionEl = document.querySelector(".profile__description");
 const newPostImageLink = document.querySelector(".card__image");
 const newPostCaption = document.querySelector(".card__title");
 
+const newPostImageLinkInput = newPostModal.querySelector("#card-image-input");
+const newPostCaptionInput = newPostModal.querySelector("#card-caption-input");
+
+function openModal(modal) {
+    modal.classList.add("modal_is-opened");
+
+}
+
+function closeModal(modal) {
+    modal.classList.remove("modal_is-opened");
+
+}
+
 editProfileBtn.addEventListener("click", function() {
-    editProfileModal.classList.add("modal_is-opened");
+   editProfileNameInput.value = profileNameEl.textContent;
+   editProfileNameDescriptionInput.value = profileDescriptionEl.textContent;
+   openModal(editProfileModal);
 }) 
 
 editProfileCloseBtn.addEventListener("click", function () {
-    editProfileNameInput.value = profileNameEl.textContent;
-    editProfileNameDesciptionInput.value = profileDescriptionEl.textContent;
-    editProfileModal.classList.remove("modal_is-opened");
+    closeModal(editProfileModal);
 
 })
 
 newPostBtn.addEventListener("click", function() {
-    newPostModal.classList.add("modal_is-opened");
+    openModal(newPostModal);
 
 })
 
 newPostCloseBtn.addEventListener("click", function() {
-    newPostModal.classList.remove("modal_is-opened");
+    closeModal(newPostModal);
 
 })
+
 
 function handleEditProfileSubmit(evt) {
     evt.preventDefault();
     profileNameEl.textContent = editProfileNameInput.value;
-    profileDescriptionEl.textContent = editProfileNameDesciptionInput.value;
-    editProfileModal.classList.remove("modal_is-opened");
+    profileDescriptionEl.textContent = editProfileNameDescriptionInput.value;
+    closeModal(editProfileModal);
 }
 
 
@@ -50,12 +64,11 @@ editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 
 function handleAddCardSubmit(evt) {
     evt.preventDefault();
-    newPostModal.classList.remove("modal_is-opened");
-    
+    console.log(newPostImageLink.value);
+    console.log(newPostCaption.value);
+    closeModal(newPostModal);
+    newPostForm.reset();
 }
-
-console.log(newPostImageLink);
-console.log(newPostCaption);
 
 newPostForm.addEventListener('submit', handleAddCardSubmit);
 
